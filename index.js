0de68e0402c0664dc9f2d3082931d7b3c6ddb56e
ravi456321 <157052803+ravi456321@users.noreply.github.com>
2024-01-19 01:10:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 const app = express();
 
+import swagger from 'swagger-ui-express';
+
+// In-app
 import DoctorController from './controllers/doctor.controller.js';
 import PatientController from './controllers/patient.controller.js';
 import ReportController from './controllers/report.controller.js';
 import jwtAuth from './middleware/auth.js';
-
+import apiDocs from './swagger.json' assert {type: 'json'};
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 
+
+server.use("/api-docs", 
+swagger.serve, 
+swagger.setup(apiDocs))
 
 // Doctor Routes
 const doctorController=new DoctorController();
@@ -19,6 +26,7 @@ const patientController=new PatientController();
 const reportController=new ReportController();
 app.post('/api/doctors/register', doctorController.register);
 app.post('/api/doctors/login', doctorController.login);
+
 
 
 // Patient Routes
