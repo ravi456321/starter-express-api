59b9ecb99c3393668ad8be7a6b1cdda073a9c3cc
ravi456321 <157052803+ravi456321@users.noreply.github.com>
2024-01-18 23:40:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import { connectToDatabase } from "./db.config.js";
+import {server} from './server.js'
+connectToDatabase
+server.listen(process.env.PORT || 3000);
