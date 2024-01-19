2f2a3d4a7570610a13b01007915b53d93f22e972
ravi456321 <157052803+ravi456321@users.noreply.github.com>
2024-01-19 10:51:59 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 //const baseUrl = process.env.MONGODB || "0.0.0.0:27017";
-const uri = "mongodb+srv://ravi:4298@atlascluster.tlcgbgf.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ravi:4298@atlascluster.tlcgbgf.mongodb.net/socialMedia?retryWrites=true&w=majority";
 export const connectToDb = async () => {
   try {
     await mongoose.connect(uri, {
