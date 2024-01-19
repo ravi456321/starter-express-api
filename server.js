e9a1a7d76ffc4470ea7cf0a3bacf3033e9699a65
ravi456321 <157052803+ravi456321@users.noreply.github.com>
2024-01-19 09:36:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import server from "./index.js";
-import { connectToDb } from "./config/db.js";
+import { connectToDb } from "./src/config/db.js";
 
 server.listen(process.env.PORT, async () => {
   await connectToDb();
